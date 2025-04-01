ea84b8737ee556c697f0bb28b60860bc545a3a68
kemplix <andrushanka23@gmailc.com>
2025-04-01 06:13:33 +0300

    Переместил движок

--- .games/Game logic.py
+++ .games/Game logic.py
@@ -1,26 +1,11 @@
-import math
+from engine import play_game
 import random
+import math
 
-def find_lcm(a, b, c):
-    return math.lcm(a, b, c)
+def generate_lcm_question():
+    numbers = [random.randint(1, 100) for _ in range(3)]
+    question = " ".join(map(str, numbers))
+    return question, math.lcm(*numbers)
 
-def play_lcm():
-    print("Welcome to the Brain Games!\n")
-    name = input("May I have your name? ")
-    print(f"Hello, {name}!\nFind the smallest common multiple of given numbers.\n")
-
-    for _ in range(3):
-        numbers = [random.randint(1, 100) for _ in range(3)]
-        correct_answer = find_lcm(*numbers)
-
-        print(f"Question: {' '.join(map(str, numbers))}")
-        user_answer = input("Your answer: ")
-
-        if user_answer == str(correct_answer):
-            print("Correct!\n")
-        else:
-            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
-            print(f"Let's try again, {name}!")
-            return
-
-    print(f"Congratulations, {name}!")
+if __name__ == "__main__":
+    play_game(generate_lcm_question, "Find the smallest common multiple of given numbers.")
--- .games/progression.py
+++ .games/progression.py
@@ -1,6 +1,7 @@
+from main.engine import play_game
 import random
 
-def generate_progression():
+def generate_progression_question():
     length = random.randint(5, 10)
     start = random.randint(1, 10)
     ratio = random.randint(2, 5)
@@ -10,23 +11,8 @@ def generate_progression():
     correct_answer = progression[hidden_index]
     progression[hidden_index] = ".."
 
-    return progression, correct_answer
+    question = " ".join(map(str, progression))
+    return question, correct_answer
 
-def play_progression():
-    print("Welcome to the Brain Games!\n")
-    name = input("May I have your name? ")
-    print(f"Hello, {name}!\nWhat number is missing in the progression?\n")
-
-    for _ in range(3):  
-        progression, correct_answer = generate_progression()
-        print(f"Question: {' '.join(map(str, progression))}")
-        user_answer = input("Your answer: ")
-
-        if user_answer == str(correct_answer):
-            print("Correct!\n")
-        else:
-            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
-            print(f"Let's try again, {name}!")
-            return
-
-    print(f"Congratulations, {name}!")
+if __name__ == "__main__":
+    play_game(generate_progression_question, "What number is missing in the progression?")
